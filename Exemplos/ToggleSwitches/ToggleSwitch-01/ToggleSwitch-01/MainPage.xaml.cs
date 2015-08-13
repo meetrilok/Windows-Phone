c4f4d56f169fba1d3cf62b4704127dbe41db40fe
Alexandre Soares <alexandre.asoares@gmail.com>
2015-08-12 21:33:59 -0300

    12082015.1

---
+++ Exemplos/ToggleSwitches/ToggleSwitch-01/ToggleSwitch-01/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace ToggleSwitch_01
+{
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void AtivaToggleSwitch(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    Progresso.IsActive = true;
+                    Progresso.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    Progresso.IsActive = false;
+                    Progresso.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+    }
+}
---
+++ Exemplos/ToggleSwitches/ToggleSwitch-02/ToggleSwitch-02/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace ToggleSwitch_02
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch ToggleSwitchBase = new ToggleSwitch();
+            ToggleSwitchBase.Header = "Ativado Programaticamente";
+            ToggleSwitchBase.OnContent = "On";
+            ToggleSwitchBase.OffContent = "Off";
+            ToggleSwitchBase.Toggled += ToggleSwitch_Toggled;
+
+            MeuStackPanel.Children.Add(ToggleSwitchBase);
+        }
+    }
+}
